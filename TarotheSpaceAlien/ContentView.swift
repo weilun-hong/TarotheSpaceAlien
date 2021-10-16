6c8977cad2204a99e8e47b04291ecb7f5042d353
weilun-hong <73873381+weilun-hong@users.noreply.github.com>
2021-10-16 16:47:45 +0800

    Initial Commit

---
+++ TarotheSpaceAlien/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TarotheSpaceAlien
+//
+//  Created by 洪偉倫 on 2021/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TarotheSpaceAlien/TarotheSpaceAlienApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarotheSpaceAlienApp.swift
+//  TarotheSpaceAlien
+//
+//  Created by 洪偉倫 on 2021/10/16.
+//
+
+import SwiftUI
+
+@main
+struct TarotheSpaceAlienApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

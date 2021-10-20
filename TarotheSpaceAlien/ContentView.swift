5a741a5b1d9a03d5897f1cc538d0fcb3614f0489
weilun-hong <eric0912e@gmail.com>
2021-10-20 20:05:46 +0800

    HW2Version2

--- TarotheSpaceAlien/ContentView.swift
+++ TarotheSpaceAlien/ContentView.swift
@@ -302,7 +302,7 @@ struct ContentView: View {
                                             Text("SPACE ALIEN")
                                                 .font(.system(size: 33.5,design:.rounded))
                                                 .fontWeight(.heavy)
-                                                .foregroundColor(Color.red)
+                                                .foregroundColor(Color.orange)
                                                 .padding()
                                                 .multilineTextAlignment(.center)
                                                 .position(x:200,y:50)
